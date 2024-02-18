db3da15575ff667b7b7f55975144c4830f8e1022
Maryangela Jaimes <maryangelajc.6@gmail.com>
2024-02-17 23:28:38 -0300

    cambios index

--- assets/js/1_funcion.js
+++ assets/js/1_funcion.js
@@ -1,6 +1,10 @@
-const example = function example(a, b, c) {
+
+// expresion de funcion
+
+const example = function (a, b, c) {
     return a + b + c;
 };
+
 
 let result = example(2, 5, 1);
 console.log(result);
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 const ele = document.getElementById("ele1")
 
-const pintar = (element, color) => (element.style.background = color)
+const pintar = (element, color) => (element.style.color = color)
 
 
 ele.addEventListener("click", () => {
